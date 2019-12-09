c88fc6447cca827d7b2467bf212447f0694c39c3
erinbilliot <billiot10232@students.apsb.org>
2019-12-09 23:55:40 +0000

    save all

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -1,10 +1,10 @@
 var background = function (window) {
     'use strict';
-    
+
     window.opspark = window.opspark || {};
     var draw = window.opspark.draw;
     var createjs = window.createjs;
-    
+
     /*
      * Create a background view for our game application
      */
@@ -16,18 +16,18 @@ var background = function (window) {
         if(!ground || typeof(ground.y) == 'undefined') {
             throw new Error("Invalid ground argument");
         }
-        
+
         // useful variables
         var canvasWidth = app.canvas.width;
         var canvasHeight = app.canvas.height;
         var groundY = ground.y;
-        
+
         // container which will be returned
         var background;
-        
+
         // ANIMATION VARIABLES HERE:
-        
-     
+
+
         // called at the start of game and whenever the page is resized
         // add objects for display in background. draws each image added to the background once
         function render() {
@@ -38,7 +38,7 @@ var background = function (window) {
             // you should modify this to suit your game
             var backgroundFill = draw.rect(canvasWidth,groundY,'black');
             background.addChild(backgroundFill);
-            
+
             // TODO: 3 - Add a moon and starfield
             var circle;
 for(var i=0;i<700;i++) {
@@ -54,18 +54,18 @@ moon.scaleX = 0.3;
 moon.scaleY = 0.3;
 background.addChild(moon);
             // TODO: 5 - Add buildings!     Q: This is before TODO 4 for a reason! Why?
-            
-            
+
+
             // TODO 4: Part 1 - Add a tree
             var tree;
             tree = draw.bitmap('img/tree.png');
 tree.x = 500;
 tree.y = 50;
 background.addChild(tree);
-            
+
         } // end of render function - DO NOT DELETE
-        
-        
+
+
         // Perform background animation
         // called on each timer "tick" - 60 times per second
         function update() {
@@ -73,25 +73,25 @@ background.addChild(tree);
             var canvasWidth = app.canvas.width;
             var canvasHeight = app.canvas.height;
             var groundY = ground.y;
-            
+
             // TODO 4: Part 2 - Move the tree!
            tree.X = tree.X + 1;
             // TODO 5: Part 2 - Parallax
-            
+
 
         } // end of update function - DO NOT DELETE
-        
-        
-        
+
+
+
         /* Make a createjs Container for the background and let it know about the render and upate functions*/
         background = new createjs.Container();
         background.resize = render;
         background.update = update;
-        
+
         /* make the background able to respond to resizing and timer updates*/
         app.addResizeable(background);
         app.addUpdateable(background);
-        
+
         /* render and return the background */
         render();
         return background;
